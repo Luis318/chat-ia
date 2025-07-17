301bec3509ea1583ae5026f52cc335886fae41a2
Luis318 <mg17032@ues.edu.sv>
2025-07-17 14:16:42 -0600

    se agrego la view

--- acad_chat/chatapi/views.py
+++ acad_chat/chatapi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ChatRequestSerializer
+from .chat_service import get_answer
 
-# Create your views here.
+class ChatView(APIView):
+
+    permission_classes = [IsAuthenticated]
+    #permission_classes = [AllowAny] 
+
+    def post(self, request):
+        serializer = ChatRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        answer = get_answer(serializer.validated_data['question'])
+        return Response({"answer": answer})
