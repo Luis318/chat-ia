c08b232f83b63c96fbe1271f13f4a6518430ec7c
Luis318 <mg17032@ues.edu.sv>
2025-07-17 14:15:13 -0600

    moviendo ingest.py

--- acad_chat/acad_chat/ingest.py
+++ acad_chat/acad_chat/ingest.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings 
 
-docs_path = pathlib.Path("documentos")
+docs_path = pathlib.Path("../documentos")
 all_chunks = []
 
 splitter = RecursiveCharacterTextSplitter(
